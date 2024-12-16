c74b4e0717f2555ac25139b2dba29b25a56aeeb4
ChupikaArtem <artem.chupika@reteno.com>
2024-12-16 19:24:25 +0200

    feat(): Added new files in the ios sdk folder

---
+++ ios/RetenoSdk.h
@@ -0,0 +1,9 @@
+#import <RetenoSdkSpec/RetenoSdkSpec.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RetenoSdk : NSObject <NativeRetenoSdkSpec>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/RetenoSdk.mm
@@ -0,0 +1,18 @@
+#import "RetenoSdk.h"
+
+@implementation RetenoSdk
+
+RCT_EXPORT_MODULE()
+
+- (void)add:(double)a b:(double)b resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+    NSNumber *result = [[NSNumber alloc] initWithInteger:a+b];
+    resolve(result);
+}
+
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
+    (const facebook::react::ObjCTurboModule::InitParams &)params
+{
+    return std::make_shared<facebook::react::NativeRetenoSdkSpecJSI>(params);
+}
+
+@end
