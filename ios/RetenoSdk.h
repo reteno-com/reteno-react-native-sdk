69af11743c9da7e3c7be17e55111d3a511cf29bc
ChupikaArtem <artem.chupika@reteno.com>
2024-12-27 20:41:21 +0200

    feat(): Fill .mm and .h files

--- ios/RetenoSdk.h
+++ ios/RetenoSdk.h
@@ -1,33 +1,44 @@
 #import <Foundation/Foundation.h>
 #import <RetenoSdkSpec/RetenoSdkSpec.h>
+#import <React/RCTEventEmitter.h>
+#import <React/RCTBridgeModule.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface RetenoSdk : NativeRetenoSdkSpecBase <NativeRetenoSdkSpec>
 
-- (void)setDeviceToken:(NSString *)deviceToken resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject;
-- (void)getInitialNotificationWithResolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject;
-- (void)setUserAttributes:(NSDictionary *)payload completion:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject;
-- (void)registerForRemoteNotifications:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject;
-- (void)setAnonymousUserAttributes:(NSDictionary *)payload resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject;
+- (void)emitOnRetenoPushReceived:(NSDictionary *)userInfo;
+- (void)emitOnRetenoPushClicked:(NSDictionary *)userInfo;
+- (void)emitOnRetenoPushButtonClicked:(NSDictionary *)actionData;
+- (void)emitBeforeInAppDisplayHandler;
+- (void)emitOnInAppDisplayHandler;
+- (void)emitBeforeInAppCloseHandler;
+- (void)emitAfterInAppCloseHandler;
+- (void)emitAddInAppMessageCustomDataHandler:(NSDictionary *)customData;
+- (void)emitOnInAppErrorHandler:(NSString *)error;
+- (void)emitUnreadMessagesCountHandler:(NSNumber *)count;
 
-- (void)pauseInAppMessages:(BOOL)isPaused resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject;
+- (void)forcePushData:(RCTPromiseResolveBlock)resolve
+               reject:(RCTPromiseRejectBlock)reject;
 
-- (void)setInAppLifecycleCallbackWithResolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject;
-
-- (void)getRecommendations:(NSDictionary *)payload resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject;
-
-- (void)logRecommendationEvent:(NSDictionary *)payload resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject;
-
-- (void)getAppInboxMessages:(NSDictionary *)payload resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject;
-
+- (void)getAppInboxMessages:(NSDictionary *)payload resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject;
+- (void)getAppInboxMessagesCount:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject;
+- (void)getRecommendations:(NSDictionary *)payload resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject;
+- (void)logEvent:(NSDictionary *)payload resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject;
+- (void)logRecommendationEvent:(NSDictionary *)payload resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject;
+- (void)markAllAsOpened:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject;
+- (void)markAsOpened:(NSArray *)messageIds resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject;
 - (void)onUnreadMessagesCountChanged;
-
-- (void)markAsOpened:(NSArray<NSString *> *)messageIds resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject;
-
-- (void)markAllAsOpened:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject;
-
-- (void)getAppInboxMessagesCount:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject;
+- (void)pauseInAppMessages:(BOOL)isPaused resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject;
+- (void)registerForRemoteNotifications:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject;
+- (void)removeInAppLifecycleCallback;
+- (void)setAnonymousUserAttributes:(NSDictionary *)payload resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject;
+- (void)setDeviceToken:(NSString *)deviceToken resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject;
+- (void)setInAppLifecycleCallback;
+- (void)setUserAttributes:(NSDictionary *)payload resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject;
+- (void)unsubscribeAllMessagesCountChanged;
+- (void)unsubscribeMessagesCountChanged;
+- (void)updatePushPermissionStatusAndroid:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject;
 
 @end
 
--- ios/RetenoSdk.mm
+++ ios/RetenoSdk.mm
@@ -1,61 +1,67 @@
 #import "RetenoSdk.h"
 
-#import <React/RCTViewManager.h>
-#import "React/RCTUIKit.h"
+#import <React/RCTLog.h>
 #import <ReactCommon/TurboModuleUtils.h>
 #import <ReactCommon/TurboModuleBinding.h>
-
-#ifdef __cplusplus
-#import <react/bridgeless/BridgelessNativeModule.h>
-#import <react/renderer/components/RCTComponentData.h>
-#import <react/bridgeless/BridgelessNativeView.h>
-#endif
-
-@interface RetenoSdk : BridgelessNativeModule
-@end
+#import "React/RCTConvert.h"
 
 @implementation RetenoSdk
 
 RCT_EXPORT_MODULE()
 
-- (void)emitOnRetenoPushReceived:([String: Any] *)userInfo {
-  [self onRetenoPushReceived:@(userInfo)]
+- (void)emitOnRetenoPushReceived:(NSDictionary *)userInfo {
+    [self sendEventWithName:@"onRetenoPushReceived" body:userInfo];
 }
 
-- (void)emitOnRetenoPushClicked:([String: Any] *)userInfo {
-  [self onRetenoPushClicked:@(userInfo)]
+- (void)emitOnRetenoPushClicked:(NSDictionary *)userInfo {
+    [self sendEventWithName:@"onRetenoPushClicked" body:userInfo];
 }
 
-- (void)emitOnRetenoPushButtonClicked:([String: Any] *)actionData {
-  [self onRetenoPushButtonClicked:@(actionData)]
+- (void)emitOnRetenoPushButtonClicked:(NSDictionary *)actionData {
+  [self sendEventWithName:@"onRetenoPushButtonClicked" body:actionData];
 }
 
 - (void)emitBeforeInAppDisplayHandler {
-  [self beforeInAppDisplayHandler]
+  [self sendEventWithName:@"beforeInAppDisplayHandler"];
 }
 
 - (void)emitOnInAppDisplayHandler {
-  [self onInAppDisplayHandler]
+  [self sendEventWithName:@"onInAppDisplayHandler"];
 }
 
 - (void)emitBeforeInAppCloseHandler {
-  [self beforeInAppCloseHandler]
+  [self sendEventWithName:@"beforeInAppCloseHandler"];
 }
 
 - (void)emitAfterInAppCloseHandler {
-  [self afterInAppCloseHandler]
+  [self sendEventWithName:@"afterInAppCloseHandler"];
 }
 
-- (void)emitAddInAppMessageCustomDataHandler:([String: Any] *)customData {
-  [self addInAppMessageCustomDataHandler:@(customData)]
+- (void)emitAddInAppMessageCustomDataHandler:(NSDictionary *)customData {
+  [self sendEventWithName:@"addInAppMessageCustomDataHandler" body:customData];
 }
 
 - (void)emitOnInAppErrorHandler:(String *)error {
-  [self onInAppErrorHandler:@(error)]
+  [self sendEventWithName:@"onInAppErrorHandler" body:error];
 }
 
 - (void)emitUnreadMessagesCountHandler:(NSNumber *)count {
-  [self unreadMessagesCountHandler:@(count)]
+  [self sendEventWithName:@"unreadMessagesCountHandler" body:count];
+}
+
+- (NSArray<NSString *> *)supportedEvents {
+    return @[
+        @"onRetenoPushReceived",
+        @"onRetenoPushClicked",
+        @"onRetenoPushButtonClicked",
+        @"beforeInAppDisplayHandler",
+        @"onInAppDisplayHandler",
+        @"beforeInAppCloseHandler",
+        @"afterInAppCloseHandler",
+        @"addInAppMessageCustomDataHandler",
+        @"onInAppErrorHandler",
+        @"unreadMessagesCountHandler"
+    ];
 }
 
 // TurboModule integration
@@ -64,5 +70,243 @@ RCT_EXPORT_MODULE()
 {
     return std::make_shared<facebook::react::NativeRetenoSdkSpecJSI>(params);
 }
+
+- (void)addListener:(NSString *)event {
+    RCTLogInfo(@"Listener added for event: %@", event);
+}
+
+- (void)removeListeners:(double)count {
+    RCTLogInfo(@"Listeners removed: %f", count);
+}
+
+- (void)forcePushData:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+    @try {
+        resolve(@{@"status": @"success"});
+    } @catch (NSException *exception) {
+        reject(@"force_push_data_failed", exception.reason, nil);
+    }
+}
+
+- (void)getInitialNotification:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+    @try {
+        NSDictionary *initialNotification = @{@"title": @"Welcome", @"body": @"Hello from Reteno!"};
+        resolve(initialNotification);
+    } @catch (NSException *exception) {
+        reject(@"get_initial_notification_failed", exception.reason, nil);
+    }
+}
+
+- (void)getAppInboxMessages:(NSDictionary *)payload resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+    NSNumber *page = payload[@"page"];
+    NSNumber *pageSize = payload[@"pageSize"];
+    NSString *statusString = payload[@"status"];
+    RetenoInboxMessagesStatus status = RetenoInboxMessagesStatusAll;
+
+    if ([statusString isEqualToString:@"OPENED"]) {
+        status = RetenoInboxMessagesStatusOpened;
+    } else if ([statusString isEqualToString:@"UNOPENED"]) {
+        status = RetenoInboxMessagesStatusUnopened;
+    }
+
+    [[Reteno inbox] downloadMessagesWithPage:page pageSize:pageSize status:status completion:^(NSArray<RetenoInboxMessage *> * _Nullable messages, NSInteger totalPages, NSError * _Nullable error) {
+        if (error) {
+            reject(@"100", @"Reteno iOS SDK Error", error);
+        } else {
+            NSMutableArray *serializedMessages = [NSMutableArray new];
+            for (RetenoInboxMessage *message in messages) {
+                [serializedMessages addObject:@{
+                    @"id": message.messageId ?: @"",
+                    @"title": message.title ?: @"",
+                    @"content": message.content ?: @"",
+                    @"imageUrl": message.imageUrl.absoluteString ?: @"",
+                    @"createdDate": @(message.createdDate.timeIntervalSince1970),
+                    @"isNew": @(message.isNew)
+                }];
+            }
+            resolve(@{
+                @"messages": serializedMessages,
+                @"totalPages": @(totalPages)
+            });
+        }
+    }];
+}
+
+- (void)getAppInboxMessagesCount:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+    [[Reteno inbox] getUnreadMessagesCountWithCompletion:^(NSInteger unreadCount, NSError * _Nullable error) {
+        if (error) {
+            reject(@"100", @"Reteno iOS SDK Error", error);
+        } else {
+            resolve(@(unreadCount));
+        }
+    }];
+}
+
+- (void)getRecommendations:(NSDictionary *)payload resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+  NSString *recomVariantId = payload[@"recomVariantId"];
+  NSArray *productIds = payload[@"productIds"];
+  NSString *categoryId = payload[@"categoryId"];
+  NSArray *filters = payload[@"filters"];
+  NSArray *fields = payload[@"fields"];
+  
+  if (!recomVariantId || !productIds || !categoryId || !filters || !fields) {
+    NSError *error = [NSError errorWithDomain:@"RetenoSdk" code:100 userInfo:@{NSLocalizedDescriptionKey: @"Invalid payload"}];
+    reject(@"100", @"Invalid payload", error);
+    return;
+  }
+  
+  // Convert filters to required type
+  NSMutableArray *recomFilters = [NSMutableArray array];
+  for (NSDictionary *filter in filters) {
+    NSString *name = filter[@"name"];
+    NSArray *values = filter[@"values"];
+    if (name && values) {
+      [recomFilters addObject:@{@"name": name, @"values": values}];
+    }
+  }
+
+  [[Reteno recommendations] getRecomsWithVariantId:recomVariantId
+                                         productIds:productIds
+                                         categoryId:categoryId
+                                            filters:recomFilters
+                                             fields:fields
+                                        completion:^(NSArray *recommendations, NSError *error) {
+    if (error) {
+      reject(@"100", @"Failed to get recommendations", error);
+    } else {
+      NSMutableArray *result = [NSMutableArray array];
+      for (id recommendation in recommendations) {
+        [result addObject:@{
+          @"productId": [recommendation productId],
+          @"name": [recommendation name],
+          @"description": [recommendation description] ?: @"",
+          @"imageUrl": [[recommendation imageUrl] absoluteString] ?: @"",
+          @"price": @([recommendation price])
+        }];
+      }
+      resolve(result);
+    }
+  }];
+}
+
+- (void)logEvent:(NSDictionary *)payload resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+  @try {
+    NSString *eventTypeKey = payload[@"eventTypeKey"];
+    NSDate *date = payload[@"date"];
+    NSDictionary *parameters = payload[@"parameters"];
+    BOOL forcePush = [payload[@"forcePush"] boolValue];
+
+    [[Reteno events] logEventWithTypeKey:eventTypeKey
+                                    date:date
+                               parameters:parameters
+                                forcePush:forcePush];
+    resolve(@{@"success": @(YES)});
+  }
+  @catch (NSException *exception) {
+    NSError *error = [NSError errorWithDomain:@"RetenoSdk" code:100 userInfo:@{NSLocalizedDescriptionKey: exception.reason}];
+    reject(@"100", @"Failed to log event", error);
+  }
+}
+
+
+- (void)logRecommendationEvent:(NSDictionary *)payload resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+  @try {
+    NSString *recomVariantId = payload[@"recomVariantId"];
+    NSString *action = payload[@"action"];
+    NSArray *productIds = payload[@"productIds"];
+
+    if (!recomVariantId || !action || !productIds) {
+      NSError *error = [NSError errorWithDomain:@"RetenoSdk" code:100 userInfo:@{NSLocalizedDescriptionKey: @"Invalid payload"}];
+      reject(@"100", @"Invalid payload", error);
+      return;
+    }
+
+    [[Reteno recommendations] logEventWithVariantId:recomVariantId action:action productIds:productIds];
+    resolve(@{@"success": @(YES)});
+  }
+  @catch (NSException *exception) {
+    NSError *error = [NSError errorWithDomain:@"RetenoSdk" code:100 userInfo:@{NSLocalizedDescriptionKey: exception.reason}];
+    reject(@"100", @"Failed to log recommendation event", error);
+  }
+}
+
+
+- (void)markAllAsOpened:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+  [[Reteno inbox] markAllAsOpenedWithCompletion:^(NSError *error) {
+    if (error) {
+      reject(@"100", @"Failed to mark all as opened", error);
+    } else {
+      resolve(@(YES));
+    }
+  }];
+}
+
+- (void)markAsOpened:(NSArray *)messageIds resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+  [[Reteno inbox] markAsOpenedWithMessageIds:messageIds completion:^(NSError *error) {
+    if (error) {
+      reject(@"100", @"Failed to mark messages as opened", error);
+    } else {
+      resolve(@(YES));
+    }
+  }];
+}
+
+- (void)onUnreadMessagesCountChanged {
+  [[Reteno inbox] onUnreadMessagesCountChanged:^(NSUInteger unreadCount) {
+    [self sendEventWithName:@"UnreadMessagesCountChanged" body:@{@"unreadCount": @(unreadCount)}];
+  }];
+}
+
+- (void)pauseInAppMessages:(BOOL)isPaused resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+  [[Reteno inAppMessages] setPaused:isPaused];
+  resolve(@{@"success": @(YES)});
+}
+
+- (void)registerForRemoteNotifications:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+  [[UIApplication sharedApplication] registerForRemoteNotifications];
+  resolve(@{@"success": @(YES)});
+}
+
+- (void)removeInAppLifecycleCallback {
+  [[Reteno inAppMessages] setLifecycleCallback:nil];
+}
+
+- (void)setAnonymousUserAttributes:(NSDictionary *)payload resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+  @try {
+    [[Reteno user] setAnonymousAttributes:payload];
+    resolve(@{@"success": @(YES)});
+  }
+  @catch (NSException *exception) {
+    NSError *error = [NSError errorWithDomain:@"RetenoSdk" code:100 userInfo:@{NSLocalizedDescriptionKey: exception.reason}];
+    reject(@"100", @"Failed to set anonymous user attributes", error);
+  }
+}
+
+- (void)setDeviceToken:(NSString *)deviceToken resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+  [[Reteno notifications] setDeviceToken:deviceToken];
+  resolve(@{@"success": @(YES)});
+}
+
+- (void)setInAppLifecycleCallback {
+  [[Reteno inAppMessages] setLifecycleCallback:^(NSString *eventName, NSDictionary *details) {
+    [self sendEventWithName:@"InAppLifecycleEvent" body:@{@"eventName": eventName, @"details": details}];
+  }];
+}
+
+- (void)setUserAttributes:(NSDictionary *)payload resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+  @try {
+    [[Reteno user] setAttributes:payload];
+    resolve(@{@"success": @(YES)});
+  }
+  @catch (NSException *exception) {
+    NSError *error = [NSError errorWithDomain:@"RetenoSdk" code:100 userInfo:@{NSLocalizedDescriptionKey: exception.reason}];
+    reject(@"100", @"Failed to set user attributes", error);
+  }
+}
+
+- (void)unsubscribeAllMessagesCountChanged {}
+
+- (void)unsubscribeMessagesCountChanged {}
+
+- (void)updatePushPermissionStatusAndroid {}
 
 @end
