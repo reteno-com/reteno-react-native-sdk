06fbd3f5d4cf67ad51940ad5cf998e74fa9be6df
ChupikaArtem <artem.chupika@reteno.com>
2025-01-28 17:19:41 +0200

    feat(): Create nitro spec

---
+++ src/spec/RetenoSdk.nitro.ts
@@ -0,0 +1,56 @@
+import { HybridObject } from 'react-native-nitro-modules';
+
+interface RetenoSdk extends HybridObject<{ ios: 'swift'; android: 'kotlin' }> {
+  logEvent: (payload: { [key: string]: unknown }) => Promise<void>;
+
+  setDeviceToken: (deviceToken: string) => Promise<void>;
+
+  setUserAttributes: (payload: { [key: string]: unknown }) => Promise<void>;
+
+  getInitialNotification: () => Promise<unknown>;
+
+  registerForRemoteNotifications: () => Promise<boolean>;
+
+  setAnonymousUserAttributes: (payload: {
+    [key: string]: unknown;
+  }) => Promise<void>;
+
+  pauseInAppMessages: (isPaused: boolean) => Promise<void>;
+
+  forcePushData: () => Promise<void>;
+
+  updatePushPermissionStatusAndroid: () => Promise<void>;
+
+  getRecommendations: (payload: {
+    [key: string]: unknown;
+  }) => Promise<Array<{ [key: string]: unknown }>>;
+
+  logRecommendationEvent: (payload: {
+    [key: string]: unknown;
+  }) => Promise<void>;
+
+  getAppInboxMessages: (payload: { [key: string]: unknown }) => Promise<{
+    messages: InboxMessage[];
+    totalPages: number;
+  }>;
+
+  markAsOpened: (messageIds: string[]) => Promise<boolean>;
+
+  markAllAsOpened: () => Promise<boolean>;
+
+  getAppInboxMessagesCount: () => Promise<number>;
+
+  onUnreadMessagesCountChanged: () => void;
+
+  setInAppLifecycleCallback: () => void;
+
+  removeInAppLifecycleCallback: () => void;
+
+  unsubscribeMessagesCountChanged: () => void;
+
+  unsubscribeAllMessagesCountChanged: () => void;
+
+  addListener: (event: string) => void;
+
+  removeListeners: (count: number) => void;
+}
