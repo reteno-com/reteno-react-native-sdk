e9f8adae842b98715c0d3acb5e4a94ab371f2b32
Serhii <serhii.poraiko@reteno.com>
2026-04-10 17:32:37 +0200

    refactor: simplify MainApplication structure and improve readability

--- example/android/app/src/main/java/com/reteno/sample/MainApplication.kt
+++ example/android/app/src/main/java/com/reteno/sample/MainApplication.kt
@@ -5,37 +5,20 @@ import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
-import com.facebook.react.ReactNativeHost
 import com.facebook.react.bridge.ReactContext
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
-import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
+import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.reteno.core.Reteno
 import com.reteno.core.RetenoConfig
-import com.reteno.core.RetenoImpl
 import com.retenosdk.RetenoReactNativeApplication
 
 class MainApplication : Application(), ReactApplication, RetenoReactNativeApplication {
 
-  private lateinit var retenoInstance: Reteno
-
-
-override val reactHost: ReactHost by lazy {
+  override val reactHost: ReactHost by lazy {
     getDefaultReactHost(
       context = applicationContext,
-      packageList =
-            PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
-            }
-
-        override fun getJSMainModuleName(): String = "index"
-
-        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
-        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+      packageList = PackageList(this).packages
     )
   }
 
@@ -43,21 +26,20 @@ override val reactHost: ReactHost by lazy {
     return Reteno.instance
   }
 
-  override val reactHost: ReactHost
-    get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
-
-
   override fun getReactContext(): ReactContext? {
-    return this.reactNativeHost.reactInstanceManager.currentReactContext
+    return reactHost.currentReactContext
   }
+
   override fun onCreate() {
     super.onCreate()
-    Reteno.initWithConfig(RetenoConfig.Builder()
-      .accessKey("630A66AF-C1D3-4F2A-ACC1-0D51C38D2B05").build())
+    Reteno.initWithConfig(
+      RetenoConfig.Builder()
+        .accessKey("630A66AF-C1D3-4F2A-ACC1-0D51C38D2B05")
+        .build()
+    )
     SoLoader.init(this, OpenSourceMergedSoMapping)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-     loadReactNative(this)
+      loadReactNative(this)
     }
   }
 }
